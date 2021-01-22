dd1b6a600763a3c719ada5d5ed4da6fe919997ff
David Lopez <davidlopez@Davids-MacBook-Pro.local>
2021-01-21 21:07:40 -0700

    Initial commit

---
+++ src/test/kotlin/com/davidlopez/datejotserver/DatejotServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.davidlopez.datejotserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatejotServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
